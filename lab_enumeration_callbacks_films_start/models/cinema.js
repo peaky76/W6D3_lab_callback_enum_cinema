096e42cd5e494f0f0678338df9c198fae10c48b6
peaky76 <robertjamespeacock@gmail.com>
2020-07-22 13:44:19 +0100

    filter films by genre test passes

--- lab_enumeration_callbacks_films_start/models/cinema.js
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -3,11 +3,15 @@ const Cinema = function (films) {
 };
 
 Cinema.prototype.getFilmTitles = function() {
-  return this.films.map( film => film.title ); 
+  return this.films.map(film => film.title); 
 };
 
 Cinema.prototype.findFilmByTitle = function(filmTitle) {
   return this.films.find(film => film.title === filmTitle);
+};
+
+Cinema.prototype.filterFilmsByGenre = function(genre) {
+  return this.films.filter(film => film.genre === genre);
 };
 
 module.exports = Cinema;
--- lab_enumeration_callbacks_films_start/specs/cinema_spec.js
+++ lab_enumeration_callbacks_films_start/specs/cinema_spec.js
@@ -38,8 +38,12 @@ describe('Cinema', function () {
     const actual = cinema.findFilmByTitle("Moonlight");
     assert.deepStrictEqual(actual, moonlight)
   });
-  
-  it('should be able to filter films by genre');
+
+  it('should be able to filter films by genre', function() {
+    const actual = cinema.filterFilmsByGenre("drama");
+    assert.deepStrictEqual(actual, [moonlight, trainspotting]);
+  });
+
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');
