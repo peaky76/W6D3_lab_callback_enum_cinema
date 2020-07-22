df1a7a2ce40e4a42ef79e14e00c9bf640ccf2c4c
peaky76 <robertjamespeacock@gmail.com>
2020-07-22 13:35:19 +0100

    can get film titles test passed

--- lab_enumeration_callbacks_films_start/models/cinema.js
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -2,4 +2,8 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.getFilmTitles = function() {
+  return this.films.map( film => film.title ); 
+};
+
 module.exports = Cinema;
